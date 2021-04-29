b557b953c7919099ed50ab34f769871495d573c3
Alexander Stensrud <alex@yuki.no>
2021-04-29 07:38:26 +0200

    Minimal emacs config

---
+++ private_dot_emacs.d/init.el
@@ -0,0 +1,7 @@
+(menu-bar-mode -1)
+(tool-bar-mode -1)
+(scroll-bar-mode -1)
+(setq inhibit-startup-screen t)
+(setq initial-scratch-message "")
+(fset 'yes-or-no-p 'y-or-n-p)
+(setq custom-file "~/.emacs.d/custom.el")
