e3df39e0d02eaa2c2119b8584dc61508c50f5fb5
Alexander Stensrud <alex@yuki.no>
2021-04-29 13:50:34 +0200

    Add buildCommand function

--- private_dot_config/tmux/zoomer/main.go
+++ private_dot_config/tmux/zoomer/main.go
@@ -11,6 +11,13 @@ var ratio = flag.Float64("ratio", 1.618, "")
 var skipSuffix = flag.String("skip", "~", "")
 var force = flag.Bool("force", false, "")
 
+func buildCommand(existing []string, args ...string) []string {
+	if len(existing) != 0 {
+		return append(append(existing, ";"), args...)
+	}
+	return args
+}
+
 func main() {
 	flag.Parse()
 
@@ -30,8 +37,13 @@ func main() {
 
 	width := int(float64(activeWindow.Width) / *ratio)
 	height := int(float64(activeWindow.Height) / *ratio)
-	cmds := []string{"resize-pane", "-t", activePane.ID,
-		"-x", fmt.Sprint(width), "-y", fmt.Sprint(height)}
+
+	resizeCommand := buildCommand(nil,
+		"resize-pane",
+		"-t", activePane.ID,
+		"-x", fmt.Sprint(width),
+		"-y", fmt.Sprint(height),
+	)
 
 	var row, col []Entry
 	for _, p := range panes {
@@ -47,18 +59,22 @@ func main() {
 	if len(row) > 1 {
 		x := fmt.Sprint((activeWindow.Width - width) / len(row))
 		for _, p := range row {
-			cmds = append(cmds, ";", "resize-pane", "-t", p.ID, "-x", x)
+			resizeCommand = buildCommand(resizeCommand, "resize-pane",
+				"-t", p.ID, "-x", x,
+			)
 		}
 	}
 
 	if len(col) > 1 {
 		y := fmt.Sprint((activeWindow.Height - height) / len(col))
 		for _, p := range col {
-			cmds = append(cmds, ";", "resize-pane", "-t", p.ID, "-y", y)
+			resizeCommand = buildCommand(resizeCommand, "resize-pane",
+				"-t", p.ID, "-y", y,
+			)
 		}
 	}
 
-	cmd := exec.Command("tmux", cmds...)
+	cmd := exec.Command("tmux", resizeCommand...)
 	if err := cmd.Run(); err != nil {
 		panic(err)
 	}
