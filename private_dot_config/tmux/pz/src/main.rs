efca354a0a1d1df8a9f677cd1a02972c2a564684
Alexander Stensrud <alex@yuki.no>
2021-05-05 08:25:12 +0200

    Updates

--- private_dot_config/tmux/pz/src/main.rs
+++ private_dot_config/tmux/pz/src/main.rs
@@ -5,7 +5,7 @@ use std::process::Command;
 use std::result;
 use std::str;
 
-type Result<T> = result::Result<T, String>;
+type Result<T> = result::Result<T, &'static str>;
 
 #[derive(Debug, Clone)]
 struct Entry {
@@ -23,12 +23,8 @@ impl Entry {
             active: fields[0] == "1",
             id: fields[1].to_string(),
             name: fields[2].to_string(),
-            width: fields[3]
-                .parse::<u64>()
-                .map_err(|_| "bad width".to_string())?,
-            height: fields[4]
-                .parse::<u64>()
-                .map_err(|_| "bad height".to_string())?,
+            width: fields[3].parse::<u64>().map_err(|_| "bad width")?,
+            height: fields[4].parse::<u64>().map_err(|_| "bad height")?,
             zoomed: fields[5] == "1",
         };
 
@@ -40,7 +36,7 @@ impl Entry {
             .split_whitespace()
             .collect::<Vec<_>>()
             .try_into()
-            .map_err(|_| "bad format".to_string())?;
+            .map_err(|_| "bad format")?;
 
         Entry::from_fields(fields)
     }
@@ -56,12 +52,12 @@ fn run_command<const S: usize>(cmd: &str, format: [&str; S]) -> Result<String> {
         .arg("-F")
         .arg(format.join(" "))
         .output()
-        .map_err(|_| "command failed".to_string())?;
+        .map_err(|_| "command failed")?;
 
     match output.status.code() {
-        Some(0) => String::from_utf8(output.stdout).map_err(|_| "bad format".to_string()),
-        Some(code) => Err(format!("exit status {}", code)),
-        None => Err("process terminated".to_string()),
+        Some(0) => String::from_utf8(output.stdout).map_err(|_| "bad format"),
+        Some(_) => Err("bad exit code"),
+        None => Err("process terminated"),
     }
 }
 
@@ -73,9 +69,7 @@ fn get_entries<const S: usize>(
         .lines()
         .map(Entry::from_str::<S>)
         .collect::<Result<Vec<_>>>()?;
-
     let (all_active, rest): (Vec<_>, Vec<_>) = all.into_iter().partition(|v| v.active);
-
     let active = all_active.into_iter().nth(0);
 
     Ok((active, rest))
@@ -109,52 +103,47 @@ fn get_panes() -> Result<(Option<Entry>, Vec<Entry>)> {
     )
 }
 
-fn main() {
-    let ratio: f64 = 1.618;
-
-    match get_windows() {
-        Ok((Some(active_window), _)) => {
-            if active_window.should_ignore() {
-                return;
-            }
-
-            match get_panes() {
-                Ok((Some(active_pane), panes)) => {
-                    let main_width = (active_window.width as f64 / ratio) as u64;
-                    let main_height = (active_window.height as f64 / ratio) as u64;
-
-                    print!(
-                        "resize-pane -t {} -x {} -y {} ; ",
-                        active_pane.id, main_width, main_height
-                    );
-
-                    let row = panes
-                        .iter()
-                        .filter(|entry| entry.height == active_pane.height)
-                        .collect::<Vec<&Entry>>();
-                    if row.len() > 1 {
-                        let x = (active_window.width - main_width) / row.len() as u64;
-                        for p in row {
-                            print!("resize-pane -t {} -x {} ; ", p.id, x);
-                        }
-                    }
-
-                    let col = panes
-                        .iter()
-                        .filter(|entry| entry.width == active_pane.width)
-                        .collect::<Vec<&Entry>>();
-                    if col.len() > 1 {
-                        let y = (active_window.height - main_height) / col.len() as u64;
-                        for p in col {
-                            print!("resize-pane -t {} -y {} ; ", p.id, y);
-                        }
-                    }
-
-                    println!();
-                }
-                _ => println!("nope"),
-            }
+fn main() -> result::Result<(), Box<dyn std::error::Error>> {
+    if let (Some(active_window), _) = get_windows()? {
+        if active_window.should_ignore() {
+            return Ok(());
         }
-        _ => println!("nope"),
+
+        if let (Some(active_pane), panes) = get_panes()? {
+            let ratio: f64 = 1.618;
+            let main_width = (active_window.width as f64 / ratio) as u64;
+            let main_height = (active_window.height as f64 / ratio) as u64;
+
+            print!(
+                "resize-pane -t {} -x {} -y {} ; ",
+                active_pane.id, main_width, main_height
+            );
+
+            let row = panes
+                .iter()
+                .filter(|entry| entry.height == active_pane.height)
+                .collect::<Vec<&Entry>>();
+            if row.len() > 1 {
+                let x = (active_window.width - main_width) / row.len() as u64;
+                for p in row {
+                    print!("resize-pane -t {} -x {} ; ", p.id, x);
+                }
+            }
+
+            let col = panes
+                .iter()
+                .filter(|entry| entry.width == active_pane.width)
+                .collect::<Vec<&Entry>>();
+            if col.len() > 1 {
+                let y = (active_window.height - main_height) / col.len() as u64;
+                for p in col {
+                    print!("resize-pane -t {} -y {} ; ", p.id, y);
+                }
+            }
+
+            println!();
+        }
     }
+
+    Ok(())
 }
